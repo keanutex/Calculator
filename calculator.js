7b394faacdb952a6faf245bfd6e9c14d6b8948bc
Keanutex <keanutex@gmail.com>
2022-07-06 12:11:32 +0200

    Using anonymous functions and another test case

--- calculator.js
+++ calculator.js
@@ -24,55 +24,89 @@ class Operation {
   toString(left, right) {}
 }
 
-class add extends Operation {
+const add = new (class extends Operation {
   perform(left, right) {
     return left + right;
   }
   toString(left, right) {
     return `(${left} + ${right})`;
   }
-}
-class subtract extends Operation {
+})();
+
+const subtract = new (class extends Operation {
   perform(left, right) {
     return left - right;
   }
   toString(left, right) {
     return `(${left} - ${right})`;
   }
-}
-class divide extends Operation {
+})();
+
+const divide = new (class extends Operation {
   perform(left, right) {
     return left / right;
   }
   toString(left, right) {
     return `(${left} ÷ ${right})`;
   }
-}
-class multiply extends Operation {
+})();
+
+const multiply = new (class extends Operation {
   perform(left, right) {
     return left * right;
   }
   toString(left, right) {
     return `(${left} x ${right})`;
   }
-}
+})();
 
-const tree = Node(
-  "÷",
+//Testing
+
+const assert = require("assert");
+
+const expression = Node(
+  subtract,
   null,
   Node(
-    "+",
+    multiply,
     null,
-    Node("", 7, null, null),
     Node(
-      "x",
+      add,
       null,
-      Node("-", null, Node("", 3, null, null), Node("", 2, null, null)),
-      Node("", 5, null, null)
-    )
+      Node(null, 50, null, null),
+      Node(divide, null, Node(null, 2, null, null), Node(null, 5, null, null))
+    ),
+    Node(null, 6, null, null)
   ),
-  Node("", 6, null, null)
+  Node(null, 10, null, null)
 );
 
-assert.strictEqual("((7 + ((3 - 2) x 5)) ÷ 6)", tree.toString());
-assert.strictEqual(2, tree.result());
+assert.strictEqual(expression.toString(), "(((50 + (2 ÷ 5)) x 6) - 10)");
+assert.strictEqual(expression.result(), 292.4);
+
+//Testing Required
+
+const tree = Node(
+  divide,
+  null,
+  Node(
+    add,
+    null,
+    Node(null, 7, null, null),
+    Node(
+      multiply,
+      null,
+      Node(
+        subtract,
+        null,
+        Node(null, 3, null, null),
+        Node(null, 2, null, null)
+      ),
+      Node(null, 5, null, null)
+    )
+  ),
+  Node(null, 6, null, null)
+);
+
+assert.strictEqual(tree.toString(), "((7 + ((3 - 2) x 5)) ÷ 6)");
+assert.strictEqual(tree.result(), 2);
